ec89da71ea246f5ec69db539166f208293178b17
DESKTOP-B6U7AQJ\Andra <gheorghe.andra.r2x@student.ucv.ro>
2024-03-11 20:47:29 +0200

    1. Added AccountController: here we have 3 Views (Login view/ Register view/ Recovery account view 2. Added 1 view for Contact 3. Modified 2 default views: Index(My home page) and Privacy View.

---
+++ BookingApplication/BookingApplication/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace BookingApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+       
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Recovery()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookingApplication/BookingApplication/Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "login",
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(
+    name: "recovery",
+    pattern: "{controller=Account}/{action=Register}/{id?}");
+app.MapControllerRoute(
+    name: "register",
+    pattern: "{controller=Account}/{action=Recovery}/{id?}");
+app.Run();
